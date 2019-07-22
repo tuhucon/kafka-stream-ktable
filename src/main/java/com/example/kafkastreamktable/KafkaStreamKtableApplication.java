4bac9605a307d4fb454f61d4270c23829fd03bf2
nguyen minh tuan <minhtuan93@yahoo.com>
2019-07-22 14:35:12 +0700

    use aggragate --> need use Materialized to provide Serdes

--- src/main/java/com/example/kafkastreamktable/KafkaStreamKtableApplication.java
+++ src/main/java/com/example/kafkastreamktable/KafkaStreamKtableApplication.java
@@ -3,15 +3,15 @@ package com.example.kafkastreamktable;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Reducer;
-import org.apache.kafka.streams.kstream.Serialized;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -39,7 +39,7 @@ public class KafkaStreamKtableApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Properties properties = new Properties();
-        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "ktable");
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "ktable1");
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
@@ -48,9 +48,12 @@ public class KafkaStreamKtableApplication implements CommandLineRunner {
 
         inputStream
                 .groupBy((k, v) -> v.getId(), Serialized.with(Serdes.Integer(), ItemSerdes.itemSerdes()))
-                .reduce((value1, value2) -> new Item(value1.getId(), value1.getCount() + value2.getCount()))
+                .aggregate(() -> 1L, (key, value, aggregate) -> {
+                    if (aggregate == null) return (long) value.getCount();
+                    return aggregate * value.getCount();
+                }, Materialized.with(Serdes.Integer(), Serdes.Long()))
                 .toStream()
-                .peek((k, v) -> System.out.println(v.toString()));
+                .peek((k, v) -> System.out.println(k.toString() + ": " + v.toString()));
 
         Topology topology = streamsBuilder.build();
         System.out.println(topology.describe());
